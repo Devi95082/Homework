a737e776f775b4e816454ac104ea777d1cc6d708
Devi95082 <deviramar95@gmail.com>
2026-04-07 21:56:24 +0530

    Exception Handling 07 March HW

---
+++ applicationexceptionhandling/InvalidUserIdException.java
@@ -0,0 +1,11 @@
+package applicationexceptionhandling;
+
+public class InvalidUserIdException extends Exception
+{
+     public InvalidUserIdException(String message)
+    {
+        super(message);
+    }
+    
+    
+}
